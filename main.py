6cf833caa0eb634f449f14f28dab922a8abb87f2
jollaman999 <admin@jollaman999.com>
2022-04-03 17:32:44 +0900

    2차원 리스트 생성과 접근

--- main.py
+++ main.py
@@ -361,3 +361,24 @@ if any(11>x for x in a):
     print("YES")
 else:
     print("NO")
+
+'''
+2차원 리스트 생성과 접근
+'''
+a=[0]*3
+print(a)
+
+a=[[0]*3 for _ in range(3)]
+print(a)
+a[0][1]=1
+print(a)
+a[1][1]=2
+print(a)
+
+for x in a:
+    print(x)
+
+for x in a:
+    for y in x:
+        print(y, end=' ')
+    print()

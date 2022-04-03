6a68b24154f73f671baeb33aa3b5d3f59eda9191
jollaman999 <admin@jollaman999.com>
2022-04-03 14:46:38 +0900

    반복문을 이용한 문제 풀이

--- main.py
+++ main.py
@@ -157,3 +157,29 @@ for i in range (1, 11):
         break
 else:
     print(11)
+
+'''
+반복문을 이용한 문제풀이
+1) 1부터 N까지 홀수출력하기
+2) 1부터 N까지의 합 구하기
+3) N의 약수출력하기
+'''
+
+# n = int(input("수를 입력하시오: "))
+# for i in range(1, n+1):
+#     if i%2 != 0:
+#         print(i, end= ' ')
+# else:
+#     print('\n')
+#
+# n = int(input("수를 입력하시오: "))
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+# else:
+#     print(sum)
+#
+# n = int(input("수를 입력하시오: "))
+# for i in range(1, n+1):
+#     if n%i == 0:
+#         print(i, end=' ')

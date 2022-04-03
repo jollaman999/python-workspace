c3b1182f29bf71f19c4ac2ef5fa3794fbd2dd306
jollaman999 <admin@jollaman999.com>
2022-04-03 15:17:25 +0900

    문자열과 내장함수

--- main.py
+++ main.py
@@ -207,3 +207,53 @@ for i in range(5):
     for j in range(5-i):
         print('*', end=' ')
     print()
+
+'''
+문자열과 내장함수
+'''
+msg="It is Time"
+print(msg.upper())
+print(msg.lower())
+print(msg)
+tmp=msg.upper()
+print(tmp)
+print(tmp.find('T'))
+print(tmp.count('T'))
+print(msg)
+print(msg[:2])
+print(msg[3:5])
+print(msg[tmp.find('S'):len(msg)])
+print(len(msg))
+for i in range(len(msg)):
+    print(msg[i], end=' ')
+print()
+
+for x in msg:
+    print(x, end=' ')
+print()
+
+for x in msg:
+    if x.isupper():
+        print(x, end= ' ')
+print()
+
+for x in msg:
+    if x.islower():
+        print(x, end= ' ')
+print()
+
+for x in msg:
+    if x.isalpha(): # 알파벳이냐?
+        print(x, end= '')
+print()
+
+tmp='AZ'
+for x in tmp:
+    print(ord(x)) # ASCII Number 출력
+
+tmp='az'
+for x in tmp:
+    print(ord(x)) # ASCII Number 출력
+
+tmp=65
+print(chr(tmp)) # ASCII Number를 문자로 출력

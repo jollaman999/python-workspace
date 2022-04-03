a36aa8ae01409119939ab7e0112ac447a780aa9e
jollaman999 <admin@jollaman999.com>
2022-04-03 15:36:57 +0900

    리스트와 내장함수(1)

--- main.py
+++ main.py
@@ -257,3 +257,58 @@ for x in tmp:
 
 tmp=65
 print(chr(tmp)) # ASCII Number를 문자로 출력
+
+'''
+리스트와 내장함수(1)
+'''
+import random as r
+b=list()
+print(b)
+
+a=[1, 2, 3, 4, 5]
+print(a)
+print(a[0])
+
+b=(list(range(1, 11)))
+print(b)
+
+c=a+b
+print(c)
+
+print(a)
+a.append(6)
+print(a)
+
+a.insert(3, 7)
+print(a)
+
+a.pop()
+print(a)
+a.pop(3)
+print(a)
+
+a.append(4)
+print(a)
+a.remove(4) # 첫번째만 없어진다.
+print(a)
+
+print(a.index(5))
+
+a=list(range(1, 11))
+print(a)
+print(sum(a))
+print(max(a))
+print(min(a))
+print(min(7, 5))
+print(min(7, 3, 5))
+print(a)
+r.shuffle(a)
+print(a)
+a.sort()
+print(a)
+a.sort(reverse=True)
+print(a)
+a.sort()
+print(a)
+a.clear()
+print(a)

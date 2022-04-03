20f46d5689dc01d042be2a1dd2d26f060ce936f7
jollaman999 <admin@jollaman999.com>
2022-04-03 17:54:32 +0900

    함수만들기

--- main.py
+++ main.py
@@ -382,3 +382,37 @@ for x in a:
     for y in x:
         print(y, end=' ')
     print()
+
+'''
+함수 만들기
+'''
+def add(a, b):
+    c=a+b
+    print(c)
+
+add(3, 2)
+add(5, 7)
+
+def add(a, b):
+    c=a+b
+    return c
+
+print(add(3, 2))
+x=add(3, 2)
+print(x)
+
+def add(a, b):
+    c=a+b
+    d=a-b
+    return c, d
+print(add(3, 2))
+
+def isPrime(x):
+    for i in range(2, x):
+        if x%i==0:
+            return False
+    return True
+a=[12, 13, 7, 9, 19]
+for y in a:
+    if isPrime(y):
+        print(y, end=' ')
